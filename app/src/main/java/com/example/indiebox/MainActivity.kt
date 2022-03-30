67b88f7af3e8dba7c26aad78e43120523fdacfbc
Julian <julian07di@gmail.com>
2022-03-30 11:30:08 -0600

    [New] Create register new app

--- app/src/main/java/com/example/indiebox/MainActivity.kt
+++ app/src/main/java/com/example/indiebox/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.indiebox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      var Boton1  = findViewById<Button>(R.id.Enter_btn)
+
+      Boton1.setOnClickListener{
+        val irRegister = Intent(this, Register_New_App::class.java)
+        startActivity(irRegister)
+    }
     }
 }
+
