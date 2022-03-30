385093d22aa637f17614698430b41b50336350d8
Ian Fallas <ian.f1999@gmail.com>
2022-03-30 11:55:45 -0600

    App y Games Activity

---
+++ app/src/main/java/com/example/indiebox/AppsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.indiebox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apps)
+    }
+}
